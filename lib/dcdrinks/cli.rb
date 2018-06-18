b7c74c5e3fe28d90506cc9c528f3ac34f4583a15
Carrie Savinell <carrielane89@gmail.com>
2018-06-18 20:12:19 +0000

    revisions to cli

--- lib/dcdrinks/cli.rb
+++ lib/dcdrinks/cli.rb
@@ -3,13 +3,11 @@ class Dcdrinks::CLI
   def call
     puts "Welcome to DC Happy Hours!"
     list_days
+    list_venues
     goodbye
   end
-  # def list_venues
-  #   @venue = Dcdrinks::Venue.day
-  #   @venue.each.with_index(1) {|ven, i| puts "#{i}. #{ven.name} - #{ven.location} - #{ven.time} - #{ven.feature} - #{ven.url}"}
-  # end
-  def list_days  #from cli
+
+  def list_days
     puts "********** Monday(1) **********"
     puts "********** Tuesday(2) **********"
     puts "********** Wednesday(3) **********"
@@ -17,7 +15,6 @@ class Dcdrinks::CLI
     puts "********** Friday(5) **********"
     puts "********** Saturday(6) **********"
     puts "********** Sunday(7) **********"
-
   end
 
   def list_venues
@@ -26,15 +23,20 @@ class Dcdrinks::CLI
       puts "Enter the number of the day for a list of happy hours or type exit."
       input = gets.strip.downcase
 
-      if input.to_i > 0 #&& (input.to_i < 8)
-        the_venue = @venue[input.to_i - 1]
+      if input.to_i > 0
+        the_venue = @venues_for_day[input.to_i - 1]
         puts "#{ven.name} - #{ven.location} - #{ven.time} - #{ven.feature} - #{ven.url}"
-      elsif input == "list"
-        list_venues
+      elsif input == "exit"
+        break
       else
-        puts "Unsure what you're asking. Enter the happy hour day or type exit."
+        puts "Unsure what you're asking. Select a happy hour list by entering a day's number or type 'exit'."
       end
     end
+  end
+
+  def day_venues
+    @venues_for_day = Dcdrinks::Venue.scrape_dchappyhours
+    @venues_for_day.each.with_index(1) {|ven, i| puts "#{i}. #{ven.name} - #{ven.location} - #{ven.time} - #{ven.feature} - #{ven.url}"}
   end
 
   def goodbye
