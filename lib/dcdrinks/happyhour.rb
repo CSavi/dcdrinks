07aa0781cfc5d2c4694fbd447c8354cc9d34e046
Carrie Savinell <carrielane89@gmail.com>
2018-06-19 21:35:06 +0000

    creating new scraping methods

--- lib/dcdrinks/happyhour.rb
+++ lib/dcdrinks/happyhour.rb
@@ -1,7 +1,15 @@
 class Dcdrinks::HappyHour
 
-  attr_accessor :name, :location, :day, :time, :feature
+  attr_accessor :name, :location, :time, :feature #:day?
 #  BASE_PATH = "./dchappyhours.com/index.phtml?selectarea=&selectday"  => creates method for days' url?
+
+  def initialize(name=nil, location=nil, time=nil, feature=nil)
+    @name = name
+    @location = location
+    @time = time
+    @feature = feature
+  end
+
 
   # def self.day_selector(selected_day) # 1-7
   #   @happyhours_for_day = selected_day
@@ -19,17 +27,25 @@ class Dcdrinks::HappyHour
 
     doc = Nokogiri::HTML(open("https://www.dchappyhours.com/#{@happyhours_for_day}"))  #this changes based off selected day
     happyhours = doc.css(".card-block")
-    #  if happyhours.css(".card-text br 'Happy Hour: '")
     happyhours.each do |details|
-      happyhour = Dcdrinks::HappyHour.new
-      happyhour.name = details.css("h2.card-title").text  #"h2.card-title a "
-      happyhour.location = details.css("p.card-text").text
-          #"p.card-text span[0]"
-      happyhour.time = details.css("p.card-text").text #"p.card-text span[1]"
-      happyhour.feature = details.css("p.card-text span.hhlistingtext").text
+      happyhour_hash = {
+        :name => details.css("h2.card-title").text  #"h2.card-title a "
+        :location => details.css("p.card-text").text
+        :time => details.css("p.card-text").text #"p.card-text span[1]"
+        :feature => details.css("p.card-text span.hhlistingtext").text
+      }
+      happyhour_array << happyhour_hash
     end
-    #return an array of happy hours
+    happyhour_array
+    #happyhour = Dcdrinks::HappyHour.new  #create happyhour object
   end
+
+  def self.scrape_individual_happyhour(weekday_url = https://www.dchappyhours.com/bussinfo.phtml?buss=634&uname=1331+Bar+%26+Lounge)
+    individual_happyhour_hash = {}
+    doc = Nokokgiri::HTML(open(weekday_url))
+    details = doc.css("")
+      details.each do |d|
+        sd = d.
 end
 
 
