33c4a5dd61b7d472f8dc264a054ffbba940c316f
Carrie Savinell <carrielane89@gmail.com>
2018-06-20 21:49:37 +0000

    added day_selector for cli

--- lib/dcdrinks/happyhour.rb
+++ lib/dcdrinks/happyhour.rb
@@ -15,11 +15,32 @@ class Dcdrinks::HappyHour
   ]
 
 
-  def self.day_selector(selected_day) # 1-7
-    @happyhours_for_day = selected_day
-  binding.pry
+  def self.day_selector(selected_day)
+    if selected_day == "1"
+      @happyhours_for_day = "Monday"
+      puts "You selected Monday."
+    elsif selected_day == "2"
+      @happyhours_for_day = "Tuesday"
+      puts "You selected Tuesday."
+    elsif selected_day == "3"
+      @happyhours_for_day = "Wednesday"
+      puts "You selected Wednesday."
+    elsif selected_day == "4"
+      @happyhours_for_day = "Thursday"
+      puts "You selected Thursday."
+    elsif selected_day == "5"
+      @happyhours_for_day = "Friday"
+      puts "You selected Friday."
+    elsif selected_day == "6"
+      @happyhours_for_day = "Saturday"
+      puts "You selected Saturday."
+    elsif selected_day == "7"
+      @happyhours_for_day = "Sunday"
+      puts "You selected Sunday."
+    else
+      puts "Selection invalid. Please select numbers 1-7."
+    end
   end
-
 
   #scrape dchappyhours.com and then return happyhours based on that data
   def self.scrape_dchappyhours  # => should i add url arg here?
